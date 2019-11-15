89417862a4a03511a35be01b0fd85693ff81f356
Fernanda <fer_zavala2711@hotmail.com>
2019-11-14 18:30:40 -0600

    Tarea Completa

---
+++ TAREA 1.py
@@ -0,0 +1,30 @@
+with open ("datos_vuelos.csv", "r") as archivo:
+    lines=archivo.readlines()
+    dmp={}
+    dmp1={}
+    dicpor={}
+    #l= cada linea en especific
+    for l in lines:
+        datos=l.split(",")
+        pais=datos[0][0:5]
+        mes=datos[1][3:5]
+        if mes in dmp:
+            dmp[mes]+=1
+        else:
+            dmp[mes]=1
+        if mes not in dmp1:
+            dmp1[mes]={} 
+        if pais in dmp1[mes]:
+            dmp1[mes][pais]+=1
+        else:
+            dmp1[mes][pais]=1
+    f=open("resultados.csv","w+")
+    for mes in dmp1:
+        for pais in dmp1[mes]:
+            por=(dmp1[mes][pais]/dmp[mes])*100
+            if por>=20:
+                
+                f.write(mes+","+pais+","+str(por)+"\n")
+    f.close()
+        
+
